6f6c2b530d32a72a871e4d20f876877e220b814e
Shoulao <shoulao.dev@gmail.com>
2020-02-07 20:55:28 +0100

    Add expl. comments

--- src/index.js
+++ src/index.js
@@ -6,14 +6,27 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
 /*
     1. npx create-react-app nazwa-apki
     2. npm install node-sass
     3. npm install eslint --save-dev
+    4. npm install -D husky lint-staged
 
+    .huskyrc.json
+        {
+        "hooks": {
+            "pre-commit": "lint-staged"
+        }
+
+    .lintstagedrc.json
+        {
+            "*.+(js|jsx)": ["eslint --fix", "git add"],
+            "*.+(json|scss|css|md)": ["prettier --write", "git add"]
+        }
+
+    5. package.json
+
+        task-runners
 */
